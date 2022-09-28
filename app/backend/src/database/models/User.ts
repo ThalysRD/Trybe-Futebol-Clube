a5034f3c62f5bbc5489d7da231e2bbf46bc828b7
ThalysRD <tgbr66@gmail.com>
2022-09-28 17:26:22 -0300

    Adiciona a excamação na declaração de atributos da classe User

--- app/backend/src/database/models/User.ts
+++ app/backend/src/database/models/User.ts
@@ -3,11 +3,11 @@ import db from '.';
 // import OtherModel from './OtherModel';
 
 class User extends Model {
-  id: number;
-  username: string;
-  role: string;
-  email: string;
-  password: string;
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
 }
 
 User.init({
