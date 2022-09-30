c3fdc14127985e0a5608620d10b807aefdce76c9
ThalysRD <tgbr66@gmail.com>
2022-09-30 16:06:45 -0300

    Adiciona o middleware na rota

--- app/backend/src/routes/userRoute.ts
+++ app/backend/src/routes/userRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import userController from '../controllers/userController';
+import verifyEmail from '../middlewares/verifyEmail';
 
 const userRoute = Router();
 
-userRoute.post('/login', userController.login);
+userRoute.post('/login', verifyEmail, userController.login);
 
 export default userRoute;
