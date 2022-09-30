963808df85dac507c073791bd193080c82a7c287
ThalysRD <tgbr66@gmail.com>
2022-09-30 15:57:26 -0300

    Adiciona a rota ao App

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import userRoute from './routes/userRoute';
 
 class App {
   public app: express.Express;
@@ -22,6 +23,7 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
+    this.app.use('/', userRoute);
   }
 
   public start(PORT: string | number):void {
