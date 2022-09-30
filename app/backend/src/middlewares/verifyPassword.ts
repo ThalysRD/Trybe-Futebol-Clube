16b600b04380f9b6c6bc988a652c6366b5525793
ThalysRD <tgbr66@gmail.com>
2022-09-30 16:27:48 -0300

    Transforma o middleware em uma classe e adiciona um middleware para verificar se a senha está correta

--- app/backend/src/middlewares/verifyPassword.ts
+++ app/backend/src/middlewares/verifyPassword.ts
@@ -1,11 +1,27 @@
 import { Request, Response, NextFunction } from 'express';
+import userModel from '../database/models/User';
+import BCryptService from '../services/BCryptService';
 
-const verifyPassword = async (req: Request, res: Response, next: NextFunction) => {
-  const data = req.body;
-  if (!data.password) {
-    return res.status(400).json({ message: 'All fields must be filled' });
-  }
-  next();
-};
+class verifyPassword {
+  static passwordExist = async (req: Request, res: Response, next: NextFunction) => {
+    const data = req.body;
+    if (!data.password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    next();
+  };
+
+  static passwordCorrect = async (req: Request, res: Response, next: NextFunction) => {
+    const data = req.body;
+    const result = await userModel.findOne({ where: { email: data.email } });
+    if (result) {
+      const verify = BCryptService.compare(data.password, result.password);
+      if (!verify) {
+        return res.status(401).json({ message: 'Incorrect email or password' });
+      }
+    }
+    next();
+  };
+}
 
 export default verifyPassword;
