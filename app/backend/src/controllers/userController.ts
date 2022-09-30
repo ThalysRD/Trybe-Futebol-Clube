e31dd94392e0bc2172fb46d863f78f0aae255dd8
ThalysRD <tgbr66@gmail.com>
2022-09-30 20:29:28 -0300

    Adiciona um controller para validar se o usuário é um admin

--- app/backend/src/controllers/userController.ts
+++ app/backend/src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import jwtService from '../services/jwtService';
 import UsersService from '../services/UserService';
 
 export default class userController {
@@ -6,5 +7,15 @@ export default class userController {
     const data = req.body;
     const result = await UsersService.login(data);
     return res.status(200).json({ token: result });
+  };
+
+  static validate = async (req: Request, res: Response) => {
+    const token = req.headers.authorization;
+    if (token) {
+      const result = jwtService.validateToken(token);
+      // const { dataUser } = result;
+      return res.status(200).json(result);
+    }
+    return res.status(404).json({ message: 'Token inválido' });
   };
 }
