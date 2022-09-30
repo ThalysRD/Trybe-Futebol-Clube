15c79c4e160eb0fc4c71e79c8b04cd960afc287d
ThalysRD <tgbr66@gmail.com>
2022-09-30 16:27:28 -0300

    Transforma o middleware em uma classe e adiciona o middleware para verificar se o email está correto

--- app/backend/src/middlewares/verifyEmail.ts
+++ app/backend/src/middlewares/verifyEmail.ts
@@ -1,11 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
+import userModel from '../database/models/User';
 
-const verifyEmail = async (req: Request, res: Response, next: NextFunction) => {
-  const data = req.body;
-  if (!data.email) {
-    return res.status(400).json({ message: 'All fields must be filled' });
-  }
-  next();
-};
+class verifyEmail {
+  static emailExist = async (req: Request, res: Response, next: NextFunction) => {
+    const data = req.body;
+    if (!data.email) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    next();
+  };
+
+  static emailIsCorrect = async (req: Request, res: Response, next: NextFunction) => {
+    const data = req.body;
+    const email = await userModel.findOne({ where: { email: data.email } });
+    if (!email) {
+      return res.status(401).json({ message: 'Incorrect email or password' });
+    }
+    next();
+  };
+}
 
 export default verifyEmail;
