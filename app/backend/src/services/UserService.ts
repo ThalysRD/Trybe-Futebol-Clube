dc8a7be6d901aa629c53a142bcc83eff855e6372
ThalysRD <tgbr66@gmail.com>
2022-09-30 16:27:57 -0300

    Remove consoles

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -6,8 +6,6 @@ import jwtService from './jwtService';
 export default class userService {
   public static login = async (data: IUser) => {
     const result = await userModel.findOne({ where: { email: data.email } });
-    console.log(result?.password);
-    console.log(data.password);
     if (result) {
       const verify = BCryptService.compare(data.password, result.password);
       if (verify) {
