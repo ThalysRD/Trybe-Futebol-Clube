f56e8e5d075d3e7dfbc523e099bf06eb89a30b03
ThalysRD <tgbr66@gmail.com>
2022-09-30 20:28:20 -0300

    Refatorando UserService

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -2,6 +2,7 @@ import userModel from '../database/models/User';
 import BCryptService from './BCryptService';
 import IUser from '../interfaces/IUser';
 import jwtService from './jwtService';
+import IUserNoPassword from '../interfaces/IUserNoPassword';
 
 export default class userService {
   public static login = async (data: IUser) => {
@@ -9,7 +10,9 @@ export default class userService {
     if (result) {
       const verify = BCryptService.compare(data.password, result.password);
       if (verify) {
-        const token = jwtService.generateToken(data.password);
+        const noPassword: IUserNoPassword = result;
+        delete noPassword.password;
+        const token = jwtService.generateToken(noPassword);
         return token;
       }
     }
