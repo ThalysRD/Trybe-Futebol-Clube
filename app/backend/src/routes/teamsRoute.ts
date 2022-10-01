8d9eb6a566a442e445644385147dfd3107f90b2b
ThalysRD <tgbr66@gmail.com>
2022-10-01 15:12:19 -0300

    Adiciona a rota que procura times por id

--- app/backend/src/routes/teamsRoute.ts
+++ app/backend/src/routes/teamsRoute.ts
@@ -4,5 +4,6 @@ import teamsController from '../controllers/teamsController';
 const teamsRoute = Router();
 
 teamsRoute.get('/teams', teamsController.allTeams);
+teamsRoute.get('/teams/:id', teamsController.teamsById);
 
 export default teamsRoute;
