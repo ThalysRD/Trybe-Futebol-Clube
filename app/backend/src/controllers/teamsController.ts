8354e3b56a4645dab8f3aa28dbc03be474382c88
ThalysRD <tgbr66@gmail.com>
2022-10-01 15:03:18 -0300

    Cria o controller que lista todos os times

---
+++ app/backend/src/controllers/teamsController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+import teamModel from '../database/models/Team';
+
+export default class teamsController {
+  static allTeams = async (_req: Request, res: Response) => {
+    const teams = await teamModel.findAll();
+    return res.status(200).json(teams);
+  };
+}
