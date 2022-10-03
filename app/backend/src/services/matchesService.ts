bf440ff88238203de989dfff6d5dbb27d18d46fc
ThalysRD <tgbr66@gmail.com>
2022-10-03 18:20:03 -0300

    Cria o service para retornar todos os times

---
+++ app/backend/src/services/matchesService.ts
@@ -0,0 +1,27 @@
+import matchModel from '../database/models/Match';
+import teamModel from '../database/models/Team';
+
+export default class matchesService {
+  static allMatches = async () => {
+    const result = await matchModel.findAll(
+      { include: [
+        { model: teamModel, as: 'teamHome', attributes: ['teamName'] },
+        { model: teamModel, as: 'teamAway', attributes: ['teamName'] },
+      ] },
+    );
+    return result;
+  };
+
+  static filtredProgress = async (inProgress: boolean) => {
+    const result = await matchModel.findAll({
+      where: {
+        inProgress,
+      },
+      include: [
+        { model: teamModel, as: 'teamHome', attributes: ['teamName'] },
+        { model: teamModel, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+    return result;
+  };
+}
