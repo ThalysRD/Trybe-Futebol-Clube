2d6a5a09ab08dfd879c806a18c152506aa62c98f
ThalysRD <tgbr66@gmail.com>
2022-10-03 17:06:22 -0300

    Cria a rota Matches

---
+++ app/backend/src/routes/matchRoute.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import matchesController from '../controllers/matchesController';
+
+const matchsRoute = Router();
+
+matchsRoute.get('/matches', matchesController.allMatches);
+// matchsRoute.post('/matches', matchesController.newMatch);
+// matchsRoute.patch('matches/:id/finish', matchesController.matchesFinish);
+
+export default matchsRoute;
