95229a1d099b54b3c69c2722ef6916abd8709ce3
ThalysRD <tgbr66@gmail.com>
2022-10-04 12:02:44 -0300

    Cria a rota para a partida

--- app/backend/src/routes/matchRoute.ts
+++ app/backend/src/routes/matchRoute.ts
@@ -4,7 +4,7 @@ import matchesController from '../controllers/matchesController';
 const matchsRoute = Router();
 
 matchsRoute.get('/matches', matchesController.allMatches);
-// matchsRoute.post('/matches', matchesController.newMatch);
+matchsRoute.post('/matches', matchesController.newMatch);
 // matchsRoute.patch('matches/:id/finish', matchesController.matchesFinish);
 
 export default matchsRoute;
