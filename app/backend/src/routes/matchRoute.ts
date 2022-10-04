3c3e476f3e03b731b92060ea655b10f89dcb6659
ThalysRD <tgbr66@gmail.com>
2022-10-04 12:35:43 -0300

    Adiciona o middleware na rota

--- app/backend/src/routes/matchRoute.ts
+++ app/backend/src/routes/matchRoute.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import verifyTeams from '../middlewares/verifyTeams';
 import matchesController from '../controllers/matchesController';
 
 const matchsRoute = Router();
 
 matchsRoute.get('/matches', matchesController.allMatches);
-matchsRoute.post('/matches', matchesController.newMatch);
-// matchsRoute.patch('matches/:id/finish', matchesController.matchesFinish);
+matchsRoute.post('/matches', verifyTeams.differentTeams, matchesController.newMatch);
+// matchsRoute.patch('matches/:id/finish', matchesController.finishMatch);
 
 export default matchsRoute;
