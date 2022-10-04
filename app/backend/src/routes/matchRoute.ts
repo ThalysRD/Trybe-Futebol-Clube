6511bf27dc8c98b6e729b35f80c88224bb85f387
ThalysRD <tgbr66@gmail.com>
2022-10-04 16:32:22 -0300

    Cria a rota patch para finalizar uma partida, e adiciona middlewares as rotas

--- app/backend/src/routes/matchRoute.ts
+++ app/backend/src/routes/matchRoute.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import verifyTeams from '../middlewares/verifyTeams';
 import matchesController from '../controllers/matchesController';
+import verifyMatch from '../middlewares/verifyMatch';
 
 const matchsRoute = Router();
 
 matchsRoute.get('/matches', matchesController.allMatches);
-matchsRoute.post('/matches', verifyTeams.differentTeams, matchesController.newMatch);
-// matchsRoute.patch('matches/:id/finish', matchesController.finishMatch);
-
+matchsRoute.post(
+  '/matches',
+  verifyMatch.tokenValidate,
+  verifyTeams.differentTeams,
+  verifyTeams.teamExists,
+  matchesController.newMatch,
+);
+matchsRoute.patch('/matches/:id/finish', verifyMatch.tokenValidate, matchesController.finishMatch);
 export default matchsRoute;
