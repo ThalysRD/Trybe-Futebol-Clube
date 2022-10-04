f8033d3dc9c04cdaecb99bb68b5b2d15d55c4a67
ThalysRD <tgbr66@gmail.com>
2022-10-04 16:31:58 -0300

    Cria um middleware para verificar que os times são diferentes e que existem no banco de dados

--- app/backend/src/middlewares/verifyTeams.ts
+++ app/backend/src/middlewares/verifyTeams.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import teamsModel from '../database/models/Team';
 
 export default class verifyTeams {
   static differentTeams = (req: Request, res: Response, next: NextFunction) => {
@@ -7,6 +8,17 @@ export default class verifyTeams {
       return res.status(401).json(
         { message: 'It is not possible to create a match with two equal teams' },
       );
+    }
+    next();
+  };
+
+  static teamExists = async (req: Request, res: Response, next: NextFunction) => {
+    const data = req.body;
+    const allTeams = await teamsModel.findAll();
+    const homeTeam = allTeams.some(({ id }) => id === data.homeTeam);
+    const awayTeam = allTeams.some(({ id }) => id === data.awayTeam);
+    if (!homeTeam || !awayTeam) {
+      return res.status(404).json({ message: 'There is no team with such id!' });
     }
     next();
   };
