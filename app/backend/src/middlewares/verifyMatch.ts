06d987af46f43b122ccb666c7d5d954a4b93c9ec
ThalysRD <tgbr66@gmail.com>
2022-10-05 16:54:42 -0300

    Atualiza middleware

--- app/backend/src/middlewares/verifyMatch.ts
+++ app/backend/src/middlewares/verifyMatch.ts
@@ -3,13 +3,18 @@ import jwtService from '../services/jwtService';
 
 export default class verifyMatch {
   static tokenValidate = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
-    if (!token) {
-      return res.status(401).json({ message: 'Token must be a valid token' });
-    }
-    const validate = jwtService.validateToken(token);
-    if (!validate.data || validate.data.role !== 'admin') {
-      return res.status(401).json({ message: 'Token must be a valid token' });
+    const message = 'Token must be a valid token';
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        return res.status(401).json({ message });
+      }
+      const validate = jwtService.validateToken(token);
+      if (!validate.data || validate.data.role !== 'admin') {
+        return res.status(401).json({ message });
+      }
+    } catch (e) {
+      return res.status(401).json({ message });
     }
     next();
   };
