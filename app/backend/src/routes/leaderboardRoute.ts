c68bae03e84e58c515fa7ded9b336c87e17c6c2c
ThalysRD <tgbr66@gmail.com>
2022-10-06 13:17:15 -0300

    E cria a rota do time visitatne

--- app/backend/src/routes/leaderboardRoute.ts
+++ app/backend/src/routes/leaderboardRoute.ts
@@ -4,5 +4,7 @@ import leaderborderController from '../controllers/leaderborderController';
 const leaderboardRoute = Router();
 
 leaderboardRoute.get('/leaderboard/home', leaderborderController.allHome);
+leaderboardRoute.get('/leaderboard/away', leaderborderController.allAway);
+// leaderboardRoute.get('/leaderboard', leaderborderController.allTeams);
 
 export default leaderboardRoute;
