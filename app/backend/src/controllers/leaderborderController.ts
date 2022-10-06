65a214beff589c9296bd271e985b5c86968796fd
ThalysRD <tgbr66@gmail.com>
2022-10-06 13:16:59 -0300

    Adiciona o controller para o time visitante

--- app/backend/src/controllers/leaderborderController.ts
+++ app/backend/src/controllers/leaderborderController.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from 'express';
-import leaderboardService from '../services/leaderboardService';
+import leaderboardServiceAway from '../services/leaderboardServiceAway';
+import leaderboardServiceHome from '../services/leaderboardServiceHome';
 
 export default class leaderborderController {
   static allHome = async (_req: Request, res: Response) => {
-    const result = await leaderboardService.homeResultSorted();
+    const result = await leaderboardServiceHome.homeResultSorted();
     return res.status(200).json(result);
   };
+
+  static allAway = async (_req: Request, res: Response) => {
+    const result = await leaderboardServiceAway.awayResultSorted();
+    return res.status(200).json(result);
+  };
+
+  // static allTeams = async (_req: Request, res: Response) => {
+  //   const result = await leaderboardService.allTeamsSorted();
+  //   return res.status(200).json(result);
+  // }
 }
